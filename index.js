db19a19b242b2b699e0d04ae7fabec17e5a89e6c
Arina <arina.ehunova@gmail.com>
2023-11-05 19:04:15 +0400

    Edit profile popup activity completed

---
+++ index.js
@@ -0,0 +1,47 @@
+const editProfileButton = document.querySelector('.profile__edit-button');
+const editProfilePopup = document.querySelector('#edit-profile');
+const closeProfilePopup = editProfilePopup.querySelector('.popup__close-button');
+const saveProfilePopup = editProfilePopup.querySelector('.popup__form');
+
+const nameProfile = document.querySelector('.profile__name-text');
+const descriptionProfile = document.querySelector('.profile__description');
+
+function openPopup(popupItem) {
+    popupItem.classList.add('popup_opened');
+}
+
+function closePopup(popupItem) {
+    popupItem.classList.remove('popup_opened');
+}
+
+function getUserInfo() {
+    const nameProfileInput = saveProfilePopup.querySelector('#profile-name');
+    const descriptionProfileInput = saveProfilePopup.querySelector('#description');
+
+    nameProfileInput.value = nameProfile.textContent;
+    descriptionProfileInput.value = descriptionProfile.textContent;
+}
+
+function updateUserInfo() {
+    const nameProfileInput = saveProfilePopup.querySelector('#profile-name');
+    const descriptionProfileInput = saveProfilePopup.querySelector('#description');
+
+    nameProfile.textContent = nameProfileInput.value;
+    descriptionProfile.textContent = descriptionProfileInput.value;
+}
+
+editProfileButton.addEventListener('click', function () {
+    getUserInfo();
+    openPopup(editProfilePopup);
+});
+
+closeProfilePopup.addEventListener('click', function () {
+    closePopup(editProfilePopup);
+});
+
+saveProfilePopup.addEventListener('submit', function(event) {
+    event.preventDefault();
+    updateUserInfo();
+    closePopup(editProfilePopup);
+});
+
